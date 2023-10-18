336a91b360d1115b2fc142075d954c6ba8ec589c
zpalfi42 <zsolt.palfi02@gmail.com>
2023-10-18 15:19:45 +0200

    Almostttt

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,14 +1,32 @@
 #! /bin/bash
 
-service mysqld start
+#service mariadb start
 
-mysql -u $DB_ROOT_USER -p$DB_ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
+#mariadb --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
+#mariadb -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
 
-mysql -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
+#mariadb -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
+#mariadb -e "FLUSH PRIVILEGES;"
+#mariadb -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
+
+#mariadb-admin -u ${DB_ROOT_USER} --password=${DB_ROOT_PASS} shutdown
+        
+#mariadb
+
+set -e
+
+/usr/sbin/mysqld &
+sleep 5
+
+#mysql -e "UPDATE mysql.user SET Host='%', Password=PASSWORD('$DB_ROOT_PASS') WHERE USER='$DB_ROOT_USER';"
+mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "SHOW DATABASES;"
+mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "CREATE DATABASE $DB_NAME;"
+mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "SHOW DATABASES;"
+mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
 mysql -e "FLUSH PRIVILEGES;"
 mysql -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
-
 mysqladmin -u ${DB_ROOT_USER} --password=${DB_ROOT_PASS} shutdown
-        
+
 mysqld
+
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
 
-if [ -f ./wp-config.php ]
-then
-	echo "Wordpress alredy exists"
-else
-	wp core download --allow-root
-	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --allow-root
-	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
-fi
+wp core download --allow-root
+wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
+wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
 
-/etc/init.d/php7.4-fpm start;
+ls .
+
+service --status-all
+
+ls /usr/sbin/
+
+/usr/sbin/php-fpm7.4 -F;
