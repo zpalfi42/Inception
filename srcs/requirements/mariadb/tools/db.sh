9ada02dac32ab2de4440985d711a9368f6b6d71a
zpalfi42 <zsolt.palfi02@gmail.com>
2023-10-18 18:22:58 +0200

    DONEgit add *! need to make better makefile

--- srcs/requirements/mariadb/tools/db.sh
+++ srcs/requirements/mariadb/tools/db.sh
@@ -1,32 +1,16 @@
 #! /bin/bash
 
-#service mariadb start
-
-#mariadb --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "CREATE DATABASE IF NOT EXISTS $DB_NAME"
-#mariadb -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS'"
-
-#mariadb -e "GRANT ALL ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
-#mariadb -e "FLUSH PRIVILEGES;"
-#mariadb -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
-
-#mariadb-admin -u ${DB_ROOT_USER} --password=${DB_ROOT_PASS} shutdown
-        
-#mariadb
-
-set -e
-
 /usr/sbin/mysqld &
 sleep 5
 
-#mysql -e "UPDATE mysql.user SET Host='%', Password=PASSWORD('$DB_ROOT_PASS') WHERE USER='$DB_ROOT_USER';"
-mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "SHOW DATABASES;"
-mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "CREATE DATABASE $DB_NAME;"
-mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "SHOW DATABASES;"
-mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
-mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%';"
-mysql -e "FLUSH PRIVILEGES;"
-mysql -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
+if [ ! -d /var/lib/mysql/${DB_NAME} ];
+then
+	mysql --user=$DB_ROOT_USER --password=$DB_ROOT_PASS -e "CREATE DATABASE $DB_NAME;"
+	mysql -e "CREATE USER '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS';"
+	mysql -e "GRANT ALL PRIVILEGES ON $DB_NAME.* TO '$DB_USER'@'%' IDENTIFIED BY '$DB_PASS' WITH GRANT OPTION;"
+	mysql -e "FLUSH PRIVILEGES;"
+	mysql -e "ALTER USER '${DB_ROOT_USER}'@'localhost' IDENTIFIED BY '${DB_ROOT_PASS}';"
+fi
+	
 mysqladmin -u ${DB_ROOT_USER} --password=${DB_ROOT_PASS} shutdown
-
-mysqld
-
+mysqld --bind-address=0.0.0.0
--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -1,9 +1,17 @@
 #! /bin/bash
 
-wp core download --allow-root
-wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
-wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
-wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
-wp theme install twentysixteen --activate --allow-root
+sleep 15
+
+if [ -f ./wp-config.php ]
+then
+	echo "exists"
+else
+	wp core download --allow-root
+	wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
+	wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
+	wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_PASS --allow-root
+	wp theme install twentysixteen --activate --allow-root
+	chmod 777 wp-content wp-content/uploads wp-content/uploads/* wp-content/uploads/*/*
+fi
 
 /usr/sbin/php-fpm7.4 -F;
