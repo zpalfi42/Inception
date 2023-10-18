5502d45d07e3bd55c1424129293166af4214f9ec
zpalfi42 <zsolt.palfi02@gmail.com>
2023-10-18 16:02:58 +0200

    Almosstttt

--- srcs/requirements/wordpress/tools/wp.sh
+++ srcs/requirements/wordpress/tools/wp.sh
@@ -4,11 +4,6 @@ wp core download --allow-root
 wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --path=/var/www/html --allow-root
 wp core install --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$WP_ADMIN_USER --admin_password=$WP_ADMIN_PASS --admin_email=$WP_ADMIN_EMAIL --skip-email --allow-root
 wp user create $WP_USER $WP_EMAIL --role=author --user_pass=$WP_USER_PASS --allow-root
-
-ls .
-
-service --status-all
-
-ls /usr/sbin/
+wp theme install twentysixteen --activate --allow-root
 
 /usr/sbin/php-fpm7.4 -F;
